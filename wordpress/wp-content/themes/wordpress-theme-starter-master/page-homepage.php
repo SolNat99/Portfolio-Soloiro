d933021c73bf5b8ccff13f49ebf81f69072ef5de
SolNat99 <natalieas01@gmail.com>
2020-03-26 14:03:58 -0700

    Finished structure of navbar

--- wordpress/wp-content/themes/wordpress-theme-starter-master/page-homepage.php
+++ wordpress/wp-content/themes/wordpress-theme-starter-master/page-homepage.php
@@ -8,9 +8,10 @@ get_header(); ?>
 	<main role="main">
 		<!-- section -->
 		<section>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <h1>This is the navbar</h1>
-
+        <nav class="navbar navbar-expand-lg navbar-light bg-custom-blue py-4">
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
         </nav>
 
 			<h1><?php the_title(); ?></h1>
